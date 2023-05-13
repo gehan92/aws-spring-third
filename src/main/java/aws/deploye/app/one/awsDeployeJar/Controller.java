61baa3c7b3f4ced8a1fd60f12ee0604d87ad52e0
P A Gehan ishanka <36468544+gehan92@users.noreply.github.com>
2023-05-13 18:50:34 +0530

    add new one

--- src/main/java/aws/deploye/app/one/awsDeployeJar/Controller.java
+++ src/main/java/aws/deploye/app/one/awsDeployeJar/Controller.java
@@ -2,12 +2,13 @@ package aws.deploye.app.one.awsDeployeJar;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+@RestController
 public class Controller {
 
 	@GetMapping("/get")
 	public String firstAwsDeployment() {
-		return "this is my first spring boot app deployment in AWS..........................";
+		return "thank you..........................";
 	}
 }
